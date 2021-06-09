b02dad52cc31713bdec15af56ad00363e6364db6
jimmykehs <jamesekehs@gmail.com>
2021-06-09 16:48:47 -0500

    Added DB function to attach tags to link

--- db/index.js
+++ db/index.js
@@ -1,24 +1,29 @@
 // Connect to DB
-const { Client } = require('pg');
-const DB_NAME = 'linkeratorDB'
-const DB_URL = process.env.DATABASE_URL || `https://localhost:5432/${ DB_NAME }`;
+const { Client } = require("pg");
+const DB_NAME = "linkeratordb";
+const DB_URL = process.env.DATABASE_URL || `https://localhost:5432/${DB_NAME}`;
 const client = new Client(DB_URL);
 
 // database methods
-async function createLink({ 
+async function createLink({
   link_name,
   link_url,
   link_image_id,
   link_view_count,
-  link_comment
+  link_comment,
 }) {
   try {
-    const { rows: [ link ] } = await client.query(`
+    const {
+      rows: [link],
+    } = await client.query(
+      `
       INSERT INTO links(link_name, link_url, link_image_id, link_view_count, link_comment) 
       VALUES($1, $2, $3, $4, $5) 
       ON CONFLICT (link_url) DO NOTHING 
       RETURNING *;
-    `, [link_name, link_url, link_image_id, link_view_count, link_comment]);
+    `,
+      [link_name, link_url, link_image_id, link_view_count, link_comment]
+    );
 
     return link;
   } catch (error) {
@@ -27,31 +32,32 @@ async function createLink({
 }
 
 //createTags
-async function createTags({tag_content}) {
+async function createTags({ tag_content }) {
   try {
-    const { rows } = await client.query(`
+    const { rows } = await client.query(
+      `
     INSERT INTO tags(tag_content)
     VALUES ($1)
     RETURNING *;
-    `, [tag_content])
-    console.log(rows)
+    `,
+      [tag_content]
+    );
+    console.log(rows);
     return rows;
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
 async function getAllLinks() {
   try {
-    console.log("Starting to get all links")
     const { rows } = await client.query(`
     SELECT *
     FROM links;
-    `)
-    console.log(rows)
-    return {rows}
+    `);
+    return rows;
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
@@ -60,38 +66,81 @@ async function getAllLinkTags() {
     const { rows } = await client.query(`
     SELECT *
     FROM link_tags;
-    `)
-    console.log(rows)
-    return rows
+    `);
+    return rows;
   } catch (error) {
-    throw error 
+    throw error;
+  }
+}
+
+async function getAllLinkTagsWithTags() {
+  try {
+    const { rows } = await client.query(`
+    SELECT *
+    FROM link_tags;
+    `);
+
+    return await attachTagstoLink(rows);
+  } catch (error) {
+    throw error;
   }
 }
 
 async function createLinkTag(linkId, tagId) {
   try {
-    await client.query(`
+    await client.query(
+      `
     INSERT INTO link_tags("linkId", "tagId")
     VALUES ($1, $2)
     ON CONFLICT ("linkId", "tagId") DO NOTHING
-    `, [linkId, tagId])
+    `,
+      [linkId, tagId]
+    );
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
 async function updateClickCount(linkId, count) {
   try {
-    await client.query(`
+    await client.query(
+      `
     UPDATE links
     SET link_view_count = $1
     WHERE ID = $2
-    `, [++count, linkId])
+    `,
+      [++count, linkId]
+    );
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
+async function attachTagstoLink(allLinks) {
+  const linkIds = allLinks.map((link) => link.id);
+  const inString = allLinks.map((_, index) => `$${index + 1}`).join(", ");
+
+  const { rows: tags } = await client.query(
+    `
+    SELECT tags.*, link_tags.*
+    FROM tags
+    JOIN link_tags ON tags.ID = link_tags."linkId"
+    WHERE link_tags."linkId" IN (${inString});
+    `,
+    linkIds
+  );
+
+  allLinks.forEach((link) => {
+    link.tags = [];
+    tags.forEach((tag) => {
+      if (tag.linkId === link.id) {
+        link.tags.push(tag);
+      }
+    });
+  });
+
+  return allLinks;
+}
 //function that joins links and tags and adds tags to said link
 
 // export
@@ -102,5 +151,7 @@ module.exports = {
   getAllLinks,
   getAllLinkTags,
   createLinkTag,
-  updateClickCount
-}
+  updateClickCount,
+  attachTagstoLink,
+  getAllLinkTagsWithTags,
+};
--- db/init_db.js
+++ db/init_db.js
@@ -7,23 +7,25 @@ const {
   getAllLinks,
   getAllLinkTags,
   createLinkTag,
-  updateClickCount
-} = require('./index');
+  updateClickCount,
+  attachTagstoLink,
+  getAllLinkTagsWithTags,
+} = require("./index");
 
 async function buildTables() {
   try {
     client.connect();
 
-    console.log("starting to drop tables")
+    console.log("starting to drop tables");
     await client.query(`
       DROP TABLE IF EXISTS link_tags;
       DROP TABLE IF EXISTS link_images;
       DROP TABLE IF EXISTS tags;
       DROP TABLE IF EXISTS links;
-     `)
-     console.log("finished dropping tables")
+     `);
+    console.log("finished dropping tables");
 
-     console.log("starting to create tables")
+    console.log("starting to create tables");
     await client.query(`
       CREATE TABLE links (
         ID SERIAL PRIMARY KEY,
@@ -48,12 +50,8 @@ async function buildTables() {
         "tagId" INT REFERENCES tags(ID) NOT NULL,
         UNIQUE ("linkId", "tagId")
       )
-    `)
-    console.log("finished creating tables")
-    // drop tables in correct order
-
-    // build tables in correct order
-
+    `);
+    console.log("finished creating tables");
   } catch (error) {
     throw error;
   }
@@ -61,42 +59,57 @@ async function buildTables() {
 
 async function populateInitialData() {
   try {
-    console.log("Starting to create links...")
-    await createLink({ 
-      link_name: 'Netflix', 
-      link_url: 'https://www.netflix.com/',
+    console.log("Starting to create links...");
+    await createLink({
+      link_name: "Netflix",
+      link_url: "https://www.netflix.com/",
       link_image_id: null,
       link_view_count: null,
-      link_comment: 'Netflix is really overpriced these days' 
+      link_comment: "Netflix is really overpriced these days",
     });
-    await createLink({ 
-      link_name: 'Hulu', 
-      link_url: 'https://www.hulu.com/welcome?orig_referrer=https%3A%2F%2Fwww.bing.com%2F',
+    await createLink({
+      link_name: "Hulu",
+      link_url:
+        "https://www.hulu.com/welcome?orig_referrer=https%3A%2F%2Fwww.bing.com%2F",
       link_image_id: null,
       link_view_count: null,
-      link_comment: 'Hulu has way better content then Netflix' 
+      link_comment: "Hulu has way better content then Netflix",
     });
-    await createLink({ 
-      link_name: 'Reverb', 
-      link_url: 'https://reverb.com/',
+    await createLink({
+      link_name: "Reverb",
+      link_url: "https://reverb.com/",
       link_image_id: null,
       link_view_count: null,
-      link_comment: 'I have no idea what this is but Nick does' 
+      link_comment: "I have no idea what this is but Nick does",
+    });
+    console.log("Finished creating links");
+
+    console.log("Creating tags");
+    await createTags({
+      tag_content: "TV/Movies",
     });
     await createTags({
-      tag_content: "TV/Movies"
-    })
+      tag_content: "sports",
+    });
     await createTags({
-      tag_content: "sports"
-    })
-    await createTags({
-      tag_content: "Music"
-    })
-    await createLinkTag(1, 1)
-    await getAllLinkTags()
-    await updateClickCount(1, 10)
-    await getAllLinks()
-    console.log("finished creating links")
+      tag_content: "Music",
+    });
+    console.log("Finished creating tags");
+
+    console.log("Creating link tags");
+    await createLinkTag(1, 1);
+    await createLinkTag(1, 2);
+    await createLinkTag(2, 3);
+    await createLinkTag(3, 2);
+    console.log("Finished creating link tags");
+
+    console.log("Attaching tags to link!");
+    const allLinks = await getAllLinks();
+    await attachTagstoLink(allLinks);
+    console.log("Finsihed attaching tags to link!");
+
+    console.log(allLinks);
+    console.log("rebuild finished!");
   } catch (error) {
     throw error;
   }
