c9527fdc2bbf55a97ab149d7fc8b1873283ab60d
ahstevens11 <austin.cards20@gmail.com>
2021-06-14 14:24:49 -0500

    The Click Count Works

--- src/api/index.js
+++ src/api/index.js
@@ -57,4 +57,15 @@ export async function createLink({ url, comment, tags }) {
   }
 }
 
-
+export async function updateLink({ id, count }) {
+  try {
+    console.log("starting to update count")
+    const data = await axios.patch(`/api/links/${id}`, {
+      count
+    })
+    console.log("finished updating the count")    
+    return data;
+  } catch (error) {
+    throw error
+  }
+}
