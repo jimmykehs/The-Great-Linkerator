34dc615101e5a5b8d820eb3855d4d22f41251147
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-06-15 20:13:32 -0500

    doesnt work

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 const { createProxyMiddleware } = require("http-proxy-middleware");
-=======
-const {createProxyMiddleware} = require('http-proxy-middleware');
-//do destructure the createProxyMiddleware above this
->>>>>>> 627920561ba9a4afa690872010369181b0934a92
 
 module.exports = function (app) {
   app.use(
