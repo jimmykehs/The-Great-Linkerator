ae1b8eec65735cecc34f665629a6d4732e156282
jimmykehs <jamesekehs@gmail.com>
2021-06-15 19:56:52 -0500

    Test

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,16 +3,26 @@ import "./SearchBar.css";
 import Modal from "react-modal";
 import { useState, useEffect } from "react";
 import { TextField } from "@material-ui/core";
-import { createLink } from '../../api'
+import { createLink, getTags } from "../../api";
 
 const SearchBar = ({ allBookmarks, setSearchResults, searchResults }) => {
+  const [allTags, setAllTags] = useState([]);
+
   const [newBookmark, setnewBookmark] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
   const [name, setName] = useState();
   const [url, setUrl] = useState();
   const [tags, setTags] = useState([]);
   const [comment, setComment] = useState();
-  
+
+  useEffect(() => {
+    async function getAllTags() {
+      const { tags } = await getTags();
+      setAllTags(tags);
+    }
+
+    getAllTags();
+  }, []);
 
   useEffect(() => {
     let results = allBookmarks.filter((result) =>
@@ -73,6 +83,11 @@ const SearchBar = ({ allBookmarks, setSearchResults, searchResults }) => {
             setSearchTerm(event.target.value);
           }}
         />
+        <select>
+          {allTags.map((tag) => {
+            return <option>{tag.tag_content}</option>;
+          })}
+        </select>
         <select
           className="Sort-Select"
           onChange={(event) => {
