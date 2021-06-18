abfd8c0b86cc86d4785a07f50ee7852cc7bd2d71
rashonwill <williams_rashon92@yahoo.com>
2021-06-17 20:54:33 -0500

    More Style

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -13,27 +13,6 @@ import {
 
 const Results = ({ searchResults }) => {
   return (
-<<<<<<< HEAD
-    <TableContainer component={Paper} id="table">
-      <Table>
-        <TableHead id="grid">
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell>Comment</TableCell>
-            <TableCell>Click Count</TableCell>
-            <TableCell>Tags</TableCell>
-            <TableCell>Creation Date</TableCell>
-            <TableCell>Delete</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {searchResults.map((bookmark) => {
-            return <BookmarkRow key={bookmark.id} bookmark={bookmark} />;
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
-=======
     <div class="Results">
       <TableContainer component={Paper}>
         <Table>
@@ -55,7 +34,6 @@ const Results = ({ searchResults }) => {
         </Table>
       </TableContainer>
     </div>
->>>>>>> b54d6f5b7380f30fbf90c12984fde2b70987a869
   );
 };
 
