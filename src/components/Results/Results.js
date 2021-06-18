16d92063632813f79edbf979cb8e5a7d4c8a14f2
jimmykehs <jamesekehs@gmail.com>
2021-06-17 20:02:11 -0500

    Fixed overflow on results

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -13,25 +13,27 @@ import {
 
 const Results = ({ searchResults }) => {
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell>Comment</TableCell>
-            <TableCell>Click Count</TableCell>
-            <TableCell>Tags</TableCell>
-            <TableCell>Creation Date</TableCell>
-            <TableCell>Delete</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {searchResults.map((bookmark) => {
-            return <BookmarkRow key={bookmark.id} bookmark={bookmark} />;
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div class="Results">
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Comment</TableCell>
+              <TableCell>Click Count</TableCell>
+              <TableCell>Tags</TableCell>
+              <TableCell>Creation Date</TableCell>
+              <TableCell>Delete</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {searchResults.map((bookmark) => {
+              return <BookmarkRow key={bookmark.id} bookmark={bookmark} />;
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 };
 
